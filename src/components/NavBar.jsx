5a3070acaa018d8173ec9e923df4737b00995d79
Riche2000 <ricardosanch2000@gmail.com>
2022-12-23 14:50:02 -0600

    Menu Phone

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import {FaBars, FaTimes} from "react-icons/fa";
 
 const NavBar = () => {
+
   const [nav, setNav] = useState(false);
 
   const links = [
@@ -38,6 +39,18 @@ const NavBar = () => {
         <li key={id} className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-150">{link}</li>
         ))}
       </ul>
+
+      <div onClick={() => setNav(!nav)} className="cursor-pointer pr-4 z-10 text-gray-500">
+        {nav ? <FaTimes size={30}/> : <FaBars size={30}/>}
+      </div>
+
+      {nav &&(
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+      {links.map(({ id, link }) => (
+        <li key={id} className="px-4 cursor-pointer capitalize py-6 text-4xl">{link}</li>
+        ))}
+      </ul>
+      )}
     </div>
   );
 };
