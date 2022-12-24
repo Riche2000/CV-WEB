8afb68adeb9c7885c12fbbb6ba77590ca5c36a48
Riche2000 <ricardosanch2000@gmail.com>
2022-12-24 14:13:57 -0600

    Experience aparece bien

--- src/components/Experiencie.jsx
+++ src/components/Experiencie.jsx
@@ -145,7 +145,7 @@ const Experience = () => {
     ];
   
     return (
-      <div name="experience" className="bg-gradient-to-b from-gray-800 to-black w-full h-scrren">
+      <div name="experience" className="bg-gradient-to-b pt-24 from-gray-800 to-black w-full h-scrren">
         <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
           <div>
             <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Experience</p>
