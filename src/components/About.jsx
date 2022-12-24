e1f608fd9b581c86b9a3b001b2062c1aa16641fa
Riche2000 <ricardosanch2000@gmail.com>
2022-12-24 11:06:37 -0600

    Languages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import mexico from "../assets/languages/mexico.png";
+import usa from "../assets/languages/usa.png";
+
 
 const About = () => {
+
+  const language =[
+    {
+      id: 1,
+      src: mexico,
+      title: "Spanish",
+      
+    },
+    {
+      id: 2,
+      src: usa,
+      title: "English",
+      
+    }
+  ]
+
   return (
     <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
            <div>
             <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About me</p>
             </div> 
-            <p className='text-xl mt-8'>
+            <p className='text-xl mt-10'>
             I was born in Mexico on July 21, 2000
             </p>
             <br />
@@ -20,7 +39,29 @@ const About = () => {
             <li>Web development using HTML5, CSS and JavaScript.</li>
             <li>Knowledge of cloud computing (Cloud), especially Microsoft Azure.</li>
             </p>
+
+            
+            <p className="py-5 mt-5 text-xl">Languages</p>
+            
+          
+          <div className="w-full grid grid-cols-2 sm:grid-cols-8 gap-8 text-center py-1 px-12 sm:px-0">
+          
+            {language.map(({id, src, title}) => (
+              <div key={id}>
+                <img src={src} alt="" className="w-20 mx-auto" />
+                <p className="mt-4">{title}</p>
+              </div>
+            ))}
+          
+</div>
+            
+            
+
+
         </div>
+
+        
+
     </div>
   )
 }
