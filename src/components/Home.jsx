70b98ec432148be86f6920cd0f132ae72942664b
Riche2000 <ricardosanch2000@gmail.com>
2022-12-24 11:21:00 -0600

    Home photo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-            <div className="flex flex-col justify-center h-full">
+            <div className="flex flex-col justify-center">
                 <h2 className="text-4xl sm:text-6xl font-bold text-white">
                     I'm a Full Stack Developer
                 </h2>
@@ -25,7 +25,7 @@ const Home = () => {
                 </div>
             </div>
             <div>
-               <img src={MyImage} alt="my profile" className="rounded-2xl mx-auto w-2/3 md:w-100" /> 
+               <img src={MyImage} alt="my profile" className="rounded-2xl mt-12 md:mt-0 mx-auto w-2/3 md:w-100" /> 
             </div>
         </div>
     </div>
