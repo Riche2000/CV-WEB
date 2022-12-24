1c09be32b3200b7fca063baa519b9ef53da2dcdb
Riche2000 <ricardosanch2000@gmail.com>
2022-12-23 19:16:01 -0600

    new icon

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
            <div>
-            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About me</p>
             </div> 
             <p className='text-xl mt-20'>
             Soy pasante de la carrera ingeniería en sistemas computacionales, en mi trayectoria como estudiante perfeccione las habilidades en tecnologías del software en lenguajes como Python, Java, JavaScript y C#. Algunas de las habilidades que he adquirido a través de mi educación en la universidad, la investigación y desarrollo de proyectos personales son:
