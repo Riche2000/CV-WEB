554ddea799df1a3f0c36c645df29870f01676159
Riche2000 <ricardosanch2000@gmail.com>
2022-12-24 18:16:50 -0600

    Se corrigio el padding top del about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,8 +21,8 @@ const About = () => {
   ]
 
   return (
-    <div name="about" className='w-full h-full pt-28 pb-16 bg-gradient-to-b from-gray-800 to-black text-white'>
-        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+    <div name="about" className='w-full h-full pt-28 pb-40 bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg my-auto p-4 mx-auto flex flex-col justify-center w-full h-full'>
            <div>
             <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About me</p>
             </div> 
