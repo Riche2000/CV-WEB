d5a38ad933195cbc66def3f50958632234762e21
Riche2000 <ricardosanch2000@gmail.com>
2023-02-24 09:25:54 -0600

    Se corrige el formato de letra de Experience

--- src/components/Experiencie.jsx
+++ src/components/Experiencie.jsx
@@ -155,7 +155,7 @@ const Experience = () => {
       <div name="experience" className="bg-gradient-to-b pt-24 from-gray-800 to-black w-full h-scrren">
         <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
           <div>
-            <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Experience</p>
+            <p className="text-4xl font-bold inline border-b-4 border-gray-500">Experience</p>
             <p className="py-6">I have knowledge in</p>
           </div>
           <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
