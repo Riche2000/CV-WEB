b3f8297d7f3ad4aee84df3b91325beadf1234523
Riche2000 <ricardosanch2000@gmail.com>
2023-02-24 15:25:42 -0600

    Contactos Listos para escritorio y movil

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 import {motion} from 'framer-motion'
+import github from "../assets/experience/github.png";
+import linkedin from "../assets/experience/linkedin.png";
+import cv from "../assets/experience/cv.png";
+
 
 const Contact = () => {
+
+  const social =[
+    {
+      id: 1,
+      src: github,
+      href: "https://github.com/Riche2000",
+    },
+    {
+      id: 2,
+      src: linkedin,
+      href: "https://www.linkedin.com/in/ricardosanch/",
+    },
+    {
+      id: 3,
+      src: cv,
+      href: "/CV_Ricardo.pdf",
+      download: true,
+    }
+  ]
+
   return (
     <div name="contact" className="w-full h-full pt-24 pb-24 bg-gradient-to-b from-black to-gray-800 p-4 text-white">
       <div className="flex flex-col my-auto p-4 justify-center max-w-screen-lg mx-auto h-full">
@@ -17,6 +41,13 @@ const Contact = () => {
             <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">Let's talk</button>
           </form>
         </motion.div>
+      </div>
+      <div className="md:hidden w-full grid grid-cols-3 sm:grid-cols-8 gap-8 text-center py-1 px-12 sm:px-0">
+      {social.map(({id, src, href, download}) => (
+              <div key={id}>
+                <a href={href} download={download} target="_blank" rel="noreferrer"><img src={src} alt="" className="py-10 w-20 mx-auto px-5"/></a>
+              </div>
+            ))}    
       </div>
     </div>
   );
